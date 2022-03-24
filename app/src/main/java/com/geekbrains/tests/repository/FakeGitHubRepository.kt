70dec7a2a627f7e9d2a383b9eabc12125221db1b
DeniZayn <0990759@gmail.com>
2022-03-24 14:17:25 +0300

    HW5

--- app/src/main/java/com/geekbrains/tests/repository/FakeGitHubRepository.kt
+++ app/src/main/java/com/geekbrains/tests/repository/FakeGitHubRepository.kt
@@ -6,31 +6,34 @@ import retrofit2.Response
 import kotlin.random.Random
 
 internal class FakeGitHubRepository: RepositoryContract {
-    override fun searchGithub(query: String,callback: RepositoryCallback) {
-        callback.handleGitHubResponse(Response.success(SearchResponse(42, listOf())))
+    override fun searchGithub(
+        query: String,
+        callback: RepositoryCallback
+    ) {
+        callback.handleGitHubResponse(Response.success(getFakeResponse()))
     }
 
-//    private fun getFakeResponse(): SearchResponse {
-//        val list: MutableList<SearchResult> = mutableListOf()
-//        for (index in 1..100) {
-//            list.add(
-//                SearchResult(
-//                    id = index,
-//                    name = "Name: $index",
-//                    fullName = "FullName: $index",
-//                    private = Random.nextBoolean(),
-//                    description = "Description: $index",
-//                    updatedAt = "Updated: $index",
-//                    size = index,
-//                    stargazersCount = Random.nextInt(100),
-//                    language = "",
-//                    hasWiki = Random.nextBoolean(),
-//                    archived = Random.nextBoolean(),
-//                    score = index.toDouble()
-//                )
-//            )
-//        }
-//        return SearchResponse(list.size, list)
-//    }
+    private fun getFakeResponse(): SearchResponse {
+        val list: MutableList<SearchResult> = mutableListOf()
+        for (index in 1..100) {
+            list.add(
+                SearchResult(
+                    id = index,
+                    name = "Name: $index",
+                    fullName = "FullName: $index",
+                    private = Random.nextBoolean(),
+                    description = "Description: $index",
+                    updatedAt = "Updated: $index",
+                    size = index,
+                    stargazersCount = Random.nextInt(100),
+                    language = "",
+                    hasWiki = Random.nextBoolean(),
+                    archived = Random.nextBoolean(),
+                    score = index.toDouble()
+                )
+            )
+        }
+        return SearchResponse(list.size, list)
+    }
 
 }
